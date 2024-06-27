c273a03ff524030479524cc9881484b03765f1a1
Rinzler <Rinzler@123>
2024-06-27 12:29:40 +0530

    Updated the Form module content

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,14 +5,16 @@ function Services() {
         Services I offer
       </h3>
       <p className="  text-center  text-xl py-2 leading-8 text-gray-600 max-sm:text-[14.7px]">
-        Skilled in full-stack web development using HTML, CSS, and JavaScript to
+        Skilled in frontend web development using HTML, CSS, and JavaScript to
         create intuitive visual designs and interactive features for websites
         and web applications. Ensures optimal user experience across desktops,
         tablets, and mobile devices through responsive design practices.
-        Proficient in React and TailwindCSS for efficient development and
-        enhanced functionality. Implements cross-browser compatibility, WCAG
-        accessibility standards, and optimizes performance with disciplined
-        coding and collaborative version control using Git.
+        Proficient in <b>React </b> and <b>TailwindCSS</b> for efficient
+        development and enhanced functionality. Implements cross-browser
+        compatibility, WCAG accessibility standards, and optimizes performance
+        with disciplined coding and collaborative version control using{" "}
+        <b>Git</b> and
+        <b> GitHub</b>.
       </p>
     </div>
   );
