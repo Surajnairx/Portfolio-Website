94e0acc607a178e3da6071b0c952d5e436796d1e
Rinzler <Rinzler@123>
2024-06-27 12:19:19 +0530

    From content update

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,22 +5,14 @@ function Services() {
         Services I offer
       </h3>
       <p className="  text-center  text-xl py-2 leading-8 text-gray-600 max-sm:text-[14.7px]">
-        Building websites from scratch using HTML, CSS, and JavaScript.
-        Designing the visual layout and interactive elements of websites or web
-        applications. Ensuring that websites function well and look good on
-        various devices and screen sizes, from desktop computers to mobile
-        phones.Utilizing front-end frameworks like React & Tailwind Css to
-        expedite development and enhance functionality.
-      </p>
-      <p className="text-center text-xl py-2 leading-8 text-gray-600 max-sm:text-[14.7px]">
-        Ensuring that websites display and function correctly across different
-        web browsers such as Chrome, Firefox, Safari, and Edge.mplementing
-        accessibility features to ensure that websites are usable by people with
-        disabilities, adhering to guidelines such as WCAG (Web Content
-        Accessibility Guidelines).Optimizing website performance by minimizing
-        loading times, reducing server requests, and optimizing code and assets.
-        Managing code changes and collaborating with other developers using
-        version control systems like Git.
+        Skilled in full-stack web development using HTML, CSS, and JavaScript to
+        create intuitive visual designs and interactive features for websites
+        and web applications. Ensures optimal user experience across desktops,
+        tablets, and mobile devices through responsive design practices.
+        Proficient in React and TailwindCSS for efficient development and
+        enhanced functionality. Implements cross-browser compatibility, WCAG
+        accessibility standards, and optimizes performance with disciplined
+        coding and collaborative version control using Git.
       </p>
     </div>
   );
